2ae33c8352efe89a83f6f959a38423049f4670b1
jokrasz <lagarto@wp.pl>
2017-07-25 19:15:59 +0200

    dodanie pierwszych plikow

---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'style.css': 'style.sass'
+  			}
+  		}
+  	},
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'build/'
+  			}]
+  		}
+  	},
+  	jshint: {
+      all: ['js/*.js']
+    },
+  	watch: {
+    scripts: {
+        files: ['*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        	},
+    	}
+	}
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'imagemin','jshint','watch']);
+};
